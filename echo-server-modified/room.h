94b6e25eb97e97f659eb6ef85bbd9ddc83be2cc2
Danny <36552981+danielmaryanski@users.noreply.github.com>
2018-12-12 10:47:29 -0500

    Removed unused files & cleaned up
    I made a backup of everything we did before, you should too before you pull this

--- echo-server-modified/room.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <user.h>
-
-//create a struct to represent a chat room
-struct room{
-  std::vector<user> user_list;  //list of all users within this room
-  char room_pass[16];           //add a room_pass
-};
--- echo-server-modified/user.h
+++
@@ -1,8 +0,0 @@
-#include <room.h>
-
-//create a struct to represent a user
-struct user{
-  char nickname[16];          //nickname of a user
-  int* connection_fd;         //socket connection between server and user
-  room* cur_room;			        //the current room the user is in
-};
